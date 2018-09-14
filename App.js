f466f6b53145361a56e604936a2794070e0efa35
bhashmitha <bhashmitha.j@gmail.com>
2018-09-14 11:20:46 +0530

    Scan reciept and respose through API

--- App.js
+++ App.js
@@ -1,123 +1,76 @@
-import React, { Component } from "react";
-import {
-  Platform,
-  StyleSheet,
-  View,
-  FlatList,
-  Text,
-  ActivityIndicator,
-  Image,
-  ToolbarAndroid,
-  TouchableOpacity
-} from "react-native";
-import Icon from 'react-native-vector-icons/Ionicons';
-import {Header} from 'react-native-elements';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-export default class Status extends Component {
-  constructor(props) {
+import React, {Component} from 'react';
+import {Platform, StyleSheet, View, Image, TouchableOpacity, Text } from 'react-native';
+//import CameraButton from './component/CameraButton/index.js';
+import Camera from './component/index.js';
+import ResponseText from './component/ResponseText/index.js';
+import Gallery from './component/Gallery/index.js';
+import Title from './component/Title.js';
+
+
+export default class App extends Component {
+  constructor(props){
     super(props);
     this.state = {
-      Loading: true,
-      musicLibrary: []
+      status: true,
+      // photo: true
     };
   }
 
-  renderItem = ({ item }) => {
-    return (
-      <View style={styles.musicRow}>
-        <View style={styles.MovieImage}>
-          <Image
-            style={{ width: 80, height: 80, borderRadius: 5 }}
-            source={{ uri: "https://image.tmdb.org/t/p/w500" + item.backdrop_path }}
-          />
-        </View>
-        <View style={{ flex: 0.6 }}>
-          <View style={{ flex: 0.5 }}>
-            <Text
-              style={{ color: "black", fontWeight: "bold", fontSize:17}}>
-              {item.title}
-            </Text>
-            <Text style={{fontSize:10}}
-              numberOfLines={2}
-              ellipsizeMode={"tail"}>
-              {item.overview}
-            </Text>
-            </View>
-            <View style={{ flex: 0.5 }}>
-              <Text style={{ color: "black" }}>{item.release_date}</Text>
-            </View>
-        </View>
-        <TouchableOpacity 
-          style={{ flex: 0.1, alignItems:'center', justifyContent: "center" }}>
-          <Icon name="md-star-outline" size={25} color="black"/>
-        </TouchableOpacity>
-      </View>
-    );
-  };
-
-  componentDidMount() {
-    fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?api_key=f9340678aa6a61a60578f56c8f272f61&language=en-US1&page=1"
-    )
-      .then(response => response.json())
-      .then(responseJson => {
-        this.setState({
-          musicLibrary: responseJson.results,
-          Loading: false
-        });
-      })
-      .catch(err => console.log(err));
+  cameraProp() {
+    this.setState({status: true});
   }
 
+  // galleryProp() {
+  //   this.setState({photo: true});
+  // }
+ 
+  openCamera =() => {
+    this.setState({status:false});
+  };
+
+  // openGallery =() =>{
+  //   this.setState({photo:false});
+  // }
+
   render() {
-    console.log(this.state.musicLibrary);
-    if (this.state.Loading) {
-      return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-          <ActivityIndicator />
+    if(this.state.status == false){
+      return <Camera onChange={this.cameraProp} />;
+    } 
+    // if(this.state.photo == false){
+    //   return <Gallery onChange={this.galleryProp} />;
+    // }
+    return(
+      <View style={{flex:1}}>
+        <View style={styles.container}>
+         <Gallery />
         </View>
-      );
-    }
-    return (
-      <View style={{flex: 1}}>
-          <Header
-            leftComponent={{
-              icon: 'menu',
-              color: 'white',
-              onPress: () => alert('ea'),
-            }}
-            centerComponent={{ text: 'MUSIC LIBRARY', style: { color: 'white', fontWeight:'bold', fontSize:17 } }}
-            rightComponent={{ icon: 'home', color: 'white', height:20}}
-            backgroundColor="black"
-            elevation={4}
-            innerContainerStyles={{flexDirection: 'row', alignItems:'center'}}
-            outerContainerStyles={{height:55}}
-          />
-        <FlatList
-          data={this.state.musicLibrary}
-          renderItem={this.renderItem}
-          style={{ flex: 1 }}
-          keyExtractor={(item, index) => index}
-        />
       </View>
     );
   }
 }
 
+
 const styles = StyleSheet.create({
-  musicRow: {
-    flex: 1,
-    flexDirection: "row",
-    margin: 5,
-    borderColor: "#ccc",
-    borderBottomWidth: 0.8,
-    marginBottom:10,
-    justifyContent: "flex-end"
+  container: {
+    flex:1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+
   },
-  MovieImage:{
-    flex: 0.3, 
-    alignItems: "flex-start", 
-    justifyContent: "center", 
-    marginBottom:15
+  scanButton: {
+    backgroundColor:"#14AED5",
+    width: '40%',
+    height: 40,
+    alignItems: 'center',
+    padding: 10
   }
-})
+});
